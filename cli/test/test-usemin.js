3f940c2411e3f6ba045826c362b26079c9645d1b
Frederick Ros <frederick.ros@gmail.com>
2012-10-03 06:51:51 +0200

    WIP

---
+++ cli/test/test-usemin.js
@@ -0,0 +1,23 @@
+/*global describe, before, after, beforeEach, afterEach, describe, it */
+var fs      = require('fs');
+var path    = require('path');
+var grunt   = require('grunt');
+var assert  = require('assert');
+var helpers = require('./helpers');
+var usemin = require('../tasks/usemin.js');
+
+var opts = grunt.cli.options;
+opts.redirect = !opts.silent;
+
+// XXX Conform to coding guidelines, mostly literral spacing stuff
+grunt.log.writeln("FRED: " + grunt.task.searchDirs.length);
+describe('usemin', function() {
+  before(helpers.directory('.test'));
+  describe('replace helper', function() {
+    it("should take into account path", function() {
+      usemin.call(grunt,grunt);
+      grunt.helper('replace',"foo bar baz", /foo/);
+    });
+  });
+});
+
