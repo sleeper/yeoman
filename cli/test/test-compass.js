573ecf1432c080570d85ba4ad130a5e740d3ea6e
Frederick Ros <frederick.ros@gmail.com>
2012-10-26 07:26:03 +0200

    WIP

--- cli/test/test-compass.js
+++ cli/test/test-compass.js
@@ -13,34 +13,42 @@ opts.redirect = !opts.silent;
 describe('compass', function() {
   before(helpers.directory('.test'));
 
-  it("should reference images from application root", function() {
+  it("should reference images from application root", function(done) {
     // Setup test environment:
     //  - copy needed fixtures: mainly a scss file referencing an image
     //  - create needed Gruntfile config to target this file
       grunt.log.muted = false;
       compass.call(grunt, grunt);
+      grunt.file.mkdir("app");
+      grunt.file.mkdir("temp");
+      grunt.file.mkdir("app/images");
+      grunt.file.mkdir("app/scss");
       helpers.gruntfile({'compass': {  dist: {
         options: {
           css_dir: 'temp/styles',
-          sass_dir: 'app/styles',
+          sass_dir: 'app/scss',
           images_dir: 'app/images',
           javascripts_dir: 'temp/scripts',
           force: true
         }
-      }});
+      }}});
 
       grunt.config.init();
 //      grunt.config('compass-handler', {html: "index.html"});
-      grunt.file.copy(path.join(__dirname,"fixtures/compass.html"), "index.html");
+      grunt.config('compass', {dist: {
+        options: {
+           css_dir: 'temp/styles',
+          sass_dir: 'app/scss',
+          images_dir: 'app/images',
+          javascripts_dir: 'temp/scripts',
+          force: true
+        }}});
+      grunt.file.copy(path.join(__dirname,"fixtures/compass.scss"), "app/scss/compass.scss");
       grunt.task.run('compass');
       grunt.task.start();
-      // Grunt config related to concat should have been changed
-      var concat_config = grunt.config('concat');
-      // Actually the fixture index.html requires to concat stuff in '/scripts/plugins.js' ...
-      // As this references a local file, the concat config should have a key named
-      // 'scripts/plugins.js' (i.e. *without* the leading /)
-      assert.ok('scripts/plugins.js' in concat_config);    
-
+      // Let's have a look at the generated file
+      var content = grunt.file.read("temp/styles/compass.css"); 
+      assert.ok( content.match(/background-image:\s+image-url("\/images\/glyphicons-halflings.png");/g) )
   });
 });
 
